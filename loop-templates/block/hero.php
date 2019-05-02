f861f75f072116c5607273be206445c94021d96d
Philip <philip_angelin@hotmail.com>
2019-05-02 18:21:16 +0200

    Now supports hero button
    And a ton of customization

--- loop-templates/block/hero.php
+++ loop-templates/block/hero.php
@@ -1,10 +1,49 @@
-<div class="jumbotron jumbotron-fluid bg-info" style="background: url(' <?php the_field('hero_background_image_url'); ?> '); background-size: 100vw;">
+<div class="jumbotron jumbotron-fluid bg-danger" style="background: url(' <?php the_field('hero_background_image_url'); ?> '); background-size: 100vw;">
 
     <div class="container">
 
         <h1 class="display-4"><strong><?php the_field('hero_title'); ?></strong></h1>
         <p class="lead"><?php the_field('hero_text'); ?></p>
+
+        <?php if (get_field('hero_button_url')) { ?>
+
+            <a class="btn btn-lg hero-button" href="<?php the_field('hero_button_url'); ?>">
+
+                <p><?php the_field('hero_button_text'); ?></p>
+
+            </a>
+
+        <?php } ?>
             
     </div> <!-- container -->
         
 </div> <!-- jumbotron -->
+
+<style>
+
+    .hero-button {
+
+        background-color: <?php the_field('hero_button_background_color'); ?>;
+        
+        border-color: <?php the_field('hero_button_border_color'); ?>;
+
+        color: <?php the_field('hero_button_text_color'); ?>;
+    }
+
+    .hero-button:hover {
+
+        background-color: <?php the_field('hero_button_backround_hover_color'); ?>;
+
+        text-decoration: none;
+    }
+
+    .hero-button p {
+        margin: 0;
+    }
+
+    .hero-button p:hover {
+
+        color: <?php the_field('hero_button_text_hover_color'); ?>;
+    }
+
+</style>
