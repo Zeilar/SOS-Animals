eef0b4fc90a14f98b2eea5d753ee16b5d0317bb5
Philip <philip_angelin@hotmail.com>
2019-05-04 19:10:14 +0200

    Moved template into file
    It was just unnecessary imo to have a template in a template in a template, makes it harder to track things

--- archive-sosa_dog.php
+++ archive-sosa_dog.php
@@ -9,6 +9,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+$dog_amount = get_field('amount_of_dogs_archive', 'option');
+
+if (!$amount) {
+    $dog_amount = 8;
+}
+
+$dog = new WP_Query([
+    'post_type' => 'sosa_dog',
+    'posts_per_page' => $dog_amount,
+    'paged' => get_query_var('paged'),
+]);
+
 get_header();
 $container = get_theme_mod( 'myportfolio_container_type' );
 ?>
@@ -21,15 +33,38 @@ $container = get_theme_mod( 'myportfolio_container_type' );
 
 			<div class="col-md col-lg-9 border content-area" id="primary">
 
-				<main class="site-main" id="main" role="main">
+                <?php
 
-					<?php while ( have_posts() ) : the_post(); ?>
+                if ($dog->have_posts()) {
 
-					<?php endwhile; // end of the loop ?>
+                ?>
 
-				</main> <!-- main -->
+                    <div class="wrapper" id="wrapper-archive-dogs">
 
-				<?php get_template_part( 'global-templates/dogs', 'archive' ); ?> <!-- dogs - for archive page only -->
+                        <div class="container">
+
+                            <div class="row">
+
+                                <?php
+                                    while ($dog->have_posts()) {
+                                        
+                                        $dog->the_post();
+
+                                        get_template_part('loop-templates/content', 'dog-archive');
+                                    }
+
+                                    wp_reset_postdata(); // ALWAYS RESET POSTDATA
+                                ?>
+                                
+                            </div> <!-- row -->
+
+                        </div> <!-- container -->
+
+                        <?php myportfolio_pagination(); ?>
+
+                    </div> <!-- wrapper -->
+
+                <?php } ?> <!-- if -->
 
 			</div> <!-- primary -->
 
