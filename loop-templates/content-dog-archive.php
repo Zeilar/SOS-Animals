6d24582e595cd7ee66601a689aea4ffa7bc885bf
Philip <philip_angelin@hotmail.com>
2019-05-05 01:50:21 +0200

    Code formatting

--- loop-templates/content-dog-archive.php
+++ loop-templates/content-dog-archive.php
@@ -11,43 +11,52 @@ foreach ($taxomonies as $taxonomy) {
 
 <div class="col-lg-4 d-flex justify-content-center <?php echo lcfirst($sex); ?>">
 
-        <div class="card rounded-lg mt-4">
+    <div class="card rounded-lg mt-4">
 
-            <img class="card-image rounded-top" src="<?php 
+        <img class="card-image rounded-top" src="<?php 
             
-            if (the_field('dog_thumbnail_frontpage_image')) {
+        if (the_field('dog_thumbnail_frontpage_image')) {
 
-                the_field('dog_thumbnail_frontpage_image');
-            }
+            the_field('dog_thumbnail_frontpage_image');
+        }
 
-            elseif (the_field('dog_thumbnail_frontpage_image_url')) {
+        elseif (the_field('dog_thumbnail_frontpage_image_url')) {
 
-                the_field('dog_thumbnail_frontpage_image_url');
-            } 
+            the_field('dog_thumbnail_frontpage_image_url');
+        } 
             
-            ?>" alt="<?php _e('Dog image', 'myportfoolio'); ?>">
+        ?>" alt="<?php _e('Dog image', 'myportfoolio'); ?>">
 
-            <!-- end of img -->
+        <!-- end of img -->
 
-            <div class="card-body">
+        <div class="card-body">
 
-                <h2 class="card-title mb-0"><?php the_title(); ?></h2>
+            <h3 class="card-title mb-0"><?php the_title(); ?></h3>
 
-            </div>
+        </div> <!-- card-body -->
 
+        <ul class="list-group list-group-flush">
 
-            <ul class="list-group list-group-flush">
+            <li class="list-group-item">
 
-                <li class="list-group-item"><a href="/dog/<?php echo lcfirst($sex); ?>"><?php _e($sex, 'myportfolio') ?><span class="pl-2 fa fa-tag"></span></a></li>
+                <a class="pl-2" href="/dog/<?php echo lcfirst($sex); ?>"><?php _e($sex, 'myportfolio') ?>
 
-            </ul>
+                    <span class="pl-2 pr-1 fa fa-tag">
 
-            <div class="card-body">
+                    </span>
 
-                <a href="<?php the_permalink(); ?>" class="btn btn-secondary"><?php _e('Read More &raquo;', 'myportfolio'); ?></a>
+                </a> <!-- taxonomy link -->
 
-            </div> <!-- card-body -->
+            </li> <!-- list-group-item -->
 
-        </div> <!-- card -->
+        </ul> <!-- list-group -->
+
+        <div class="card-body">
+
+            <a href="<?php the_permalink(); ?>" class="btn btn-secondary"><?php _e('Read More &raquo;', 'myportfolio'); ?></a>
+
+        </div> <!-- card-body -->
+
+    </div> <!-- card -->
     
 </div> <!-- col -->
