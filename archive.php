1b474fdeaf4d86e1a98670bf26b9814af21160c3
Philip <philip_angelin@hotmail.com>
2019-05-06 13:29:49 +0200

    Fixed archive.php

--- archive.php
+++ archive.php
@@ -22,31 +22,26 @@ $container = get_theme_mod( 'myportfolio_container_type' );
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
-			<main class="site-main" id="main">
 
 				<?php if ( have_posts() ) : ?>
 
 					<header class="page-header">
-						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
 
-					<?php /* Start the Loop */ ?>
+						<?php
+
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+
+							the_archive_description( '<div class="taxonomy-description">', '</div>' );
+							
+						?>
+
+					</header> <!-- page-header -->
+
 					<?php while ( have_posts() ) : the_post(); ?>
 
 						<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
+							get_template_part( 'loop-templates/content', get_post_format() );
 						?>
 
 					<?php endwhile; ?>
@@ -57,18 +52,14 @@ $container = get_theme_mod( 'myportfolio_container_type' );
 
 				<?php endif; ?>
 
-			</main><!-- #main -->
-
-			<!-- The pagination component -->
 			<?php myportfolio_pagination(); ?>
 
-			<!-- Do the right sidebar check -->
 			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
-		</div> <!-- .row -->
+		</div> <!-- row -->
 
-	</div><!-- #content -->
+	</div> <!-- content -->
 
-	</div><!-- #archive-wrapper -->
+	</div> <!-- archive-wrapper -->
 
 <?php get_footer(); ?>
