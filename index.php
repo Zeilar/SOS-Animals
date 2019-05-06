52466a1f9c1756ffdf41ae2716dd46db9ce02bbd
Philip <philip_angelin@hotmail.com>
2019-05-06 13:25:39 +0200

    Fixed index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * The template for displaying the front page.
  *
  * @package myportfolio
  */
@@ -18,59 +12,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 
 $container = get_theme_mod( 'myportfolio_container_type' );
+
 ?>
 
-<?php if ( is_front_page() && is_home() ) : ?>
-	<?php get_template_part( 'global-templates/hero' ); ?>
-<?php endif; ?>
+<div class="wrapper" id="frontpage-wrapper">
 
-<div class="wrapper" id="index-wrapper">
-
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr( $container ); ?>-fluid" id="content" tabindex="-1">
 
 		<div class="row">
 
-			<!-- Do the left sidebar check and opens the primary div -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
 			<main class="site-main" id="main">
 
-				<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php /* Start the Loop */ ?>
+					<?php get_template_part( 'loop-templates/content', 'frontpage' ); ?>
 
-					<?php while ( have_posts() ) : the_post(); ?>
+				<?php endwhile; // end of the loop ?>
 
-						<?php
+			</main> <!-- main -->
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-						?>
+		</div> <!-- row -->
 
-					<?php endwhile; ?>
+	</div> <!-- content -->
 
-				<?php else : ?>
+</div> <!-- wrapper -->
 
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+<?php get_template_part( 'global-templates/hero' ); ?> <!-- hero -->
 
-				<?php endif; ?>
+<?php get_template_part( 'global-templates/usps' ); ?> <!-- unique selling points -->
 
-			</main><!-- #main -->
-
-			<!-- The pagination component -->
-			<?php myportfolio_pagination(); ?>
-
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
-		</div><!-- .row -->
-
-	</div><!-- #content -->
-
-</div><!-- #index-wrapper -->
-
+<?php get_template_part( 'global-templates/dogs', 'frontpage' ); ?> <!-- dogs - for frontpage only -->
+ 
 <?php get_footer(); ?>
