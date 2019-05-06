cb81c2747dbc0a1006c58a47e71fff9c12b4bced
Philip <philip_angelin@hotmail.com>
2019-05-06 13:30:33 +0200

    Fixed page.php

--- page.php
+++ page.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * The template for displaying the front page.
  *
  * @package myportfolio
  */
@@ -20,39 +15,32 @@ $container = get_theme_mod( 'myportfolio_container_type' );
 
 ?>
 
-<div class="wrapper" id="page-wrapper">
+<div class="wrapper" id="frontpage-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr( $container ); ?>-fluid" id="content" tabindex="-1">
 
 		<div class="row">
-
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
 			<main class="site-main" id="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+					<?php get_template_part( 'loop-templates/content', 'frontpage' ); ?>
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
+				<?php endwhile; // end of the loop ?>
 
-				<?php endwhile; // end of the loop. ?>
+			</main> <!-- main -->
 
-			</main><!-- #main -->
+		</div> <!-- row -->
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+	</div> <!-- content -->
 
-		</div><!-- .row -->
+</div> <!-- wrapper -->
 
-	</div><!-- #content -->
+<?php get_template_part( 'global-templates/hero' ); ?> <!-- hero -->
 
-</div><!-- #page-wrapper -->
+<?php get_template_part( 'global-templates/usps' ); ?> <!-- unique selling points -->
 
+<?php get_template_part( 'global-templates/dogs', 'frontpage' ); ?> <!-- dogs - for frontpage only -->
+ 
 <?php get_footer(); ?>
