b83caae094fc3f8722af2d6874e54fecd0a2e25a
Philip <philip_angelin@hotmail.com>
2019-05-08 01:36:35 +0200

    Added translation
    Just in case the plugin doesn't do it by itself

--- loop-templates/content-contact.php
+++ loop-templates/content-contact.php
@@ -21,7 +21,7 @@ if (!$contact_form_id) {
 
         <div class="wrapper" id="contact-form-wrapper">
 
-            <?php the_field('contact_form_shortcode'); ?>
+            <?php __(the_field('contact_form_shortcode'), 'myportfolio'); ?>
 
         </div> <!-- wrapper -->
 
