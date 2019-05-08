b80c0e160613dec9e8dd5d11e037181b9d886cde
Philip Angelin <philip_angelin@hotmail.com>
2019-05-08 10:32:11 +0200

    Fixed CPT categories relation

--- functions.php
+++ functions.php
@@ -40,3 +40,23 @@ foreach ( $understrap_includes as $file ) {
 	}
 	require_once $filepath;
 }
+
+function my_cptui_add_post_types_to_archives( $query ) { // this adds Custom Post Types into the WP category system
+	// We do not want unintended consequences.
+	if ( is_admin() || ! $query->is_main_query() ) {
+		return;    
+	}
+
+	if ( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+		$cptui_post_types = cptui_get_post_type_slugs();
+
+		$query->set(
+			'post_type',
+			array_merge(
+				array( 'post' ),
+				$cptui_post_types
+			)
+		);
+	}
+}
+add_filter( 'pre_get_posts', 'my_cptui_add_post_types_to_archives' );
