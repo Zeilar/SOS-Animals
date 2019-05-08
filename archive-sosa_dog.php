fed0b64e7f1e1363cf1d06b99f6d41145c7f296d
Philip Angelin <philip_angelin@hotmail.com>
2019-05-08 10:04:28 +0200

    Fixed pagination styling

--- archive-sosa_dog.php
+++ archive-sosa_dog.php
@@ -9,18 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-$dog_amount = get_field('amount_of_dogs_archive', 'option');
-
-if (!$dog_amount) {
-    $dog_amount = 6;
-}
-
-$dog = new WP_Query([
-    'post_type' => 'sosa_dog',
-    'posts_per_page' => $dog_amount,
-    'paged' => (get_query_var( 'paged')) ? get_query_var( 'paged') : 1
-]);
-
 get_header();
 $container = get_theme_mod( 'myportfolio_container_type' );
 ?>
---
+++ category.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package myportfolio
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+$container = get_theme_mod( 'myportfolio_container_type' );
+?>
+
+<div class="wrapper" id="archive-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<?php get_template_part('global-templates/left-sidebar-check'); ?>
+
+				<?php if (have_posts()) : ?>
+
+					<header class="page-header border">
+
+						<?php
+
+							the_archive_title('<h1 class="page-title pl-1 pr-1">', '</h1>');
+
+							the_archive_description( '<div class="taxonomy-description">', '</div>' );
+							
+						?>
+
+					</header> <!-- page-header -->
+
+					<?php while (have_posts()) : the_post(); ?>
+
+						<?php get_template_part( 'loop-templates/content', get_post_format()); ?>
+
+					<?php endwhile; ?>
+
+				<?php else : ?>
+
+					<?php get_template_part('loop-templates/content', 'none'); ?>
+
+				<?php endif; ?>
+
+			<?php get_template_part('global-templates/right-sidebar-check'); ?>
+
+		</div> <!-- row -->
+
+		<?php myportfolio_pagination(); ?>
+
+	</div> <!-- content -->
+
+</div> <!-- archive-wrapper -->
+
+<?php get_footer(); ?>
