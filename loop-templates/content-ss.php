d40785122ccabbe413fcdaac6a98701d994acb40
Philip <philip_angelin@hotmail.com>
2019-05-09 07:04:58 +0200

    Fixed center alignment on hero

--- loop-templates/content-ss.php
+++ loop-templates/content-ss.php
@@ -1,4 +1,5 @@
-<div class="col d-flex justify-content-center" style="background: url('<?php echo get_field('ss_background_image') . ' ';  the_field('ss_background_image_url'); ?>'); background-size: 100% 100%;">
+<div class="col d-flex justify-content-center" style="background: url('<?php echo get_field('ss_background_image') . ' '; 
+the_field('ss_background_image_url'); ?>'); background-size: 100% 100%;">
 
     <h1 class="ss-title pl-1 pr-1"><?php the_field('ss_title'); ?></h1>
             
