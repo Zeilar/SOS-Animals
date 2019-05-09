ea38c2f5afb1d0038e1fb55ad3246e3eab7da567
Philip <philip_angelin@hotmail.com>
2019-05-09 07:53:28 +0200

    Fixed classes again

--- loop-templates/content-ss.php
+++ loop-templates/content-ss.php
@@ -4,7 +4,7 @@ the_field('ss_background_image_url'); ?>'); background-size: 100% 100%;">
 
     <div class="ss-content container">
 
-    <h1 class="ss-title w-100 text-center pl-1 pr-1" style="height: 3rem;"><?php the_title(); ?></h1>
+    <h1 class="ss-title text-center" style="height: 3rem;"><?php the_title(); ?></h1>
 
         <?php the_content(); ?>
 
