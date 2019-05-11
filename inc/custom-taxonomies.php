df42fc52e463eca237051b83910a7af91c5c66a7
Philip <philip_angelin@hotmail.com>
2019-05-11 07:12:16 +0200

    Exported CPT/ACF

--- inc/custom-taxonomies.php
+++ inc/custom-taxonomies.php
@@ -29,5 +29,61 @@ $args = array(
     "show_in_quick_edit" => false,
     );
 register_taxonomy( "sex", array( "sosa_dog" ), $args );
+
+/**
+ * Taxonomy: Country.
+ */
+
+$labels = array(
+    "name" => __( "Country", "sosa" ),
+    "singular_name" => __( "Country", "sosa" ),
+);
+
+$args = array(
+    "label" => __( "Country", "sosa" ),
+    "labels" => $labels,
+    "public" => true,
+    "publicly_queryable" => true,
+    "hierarchical" => true,
+    "show_ui" => true,
+    "show_in_menu" => true,
+    "show_in_nav_menus" => true,
+    "query_var" => true,
+    "rewrite" => array( 'slug' => 'country', 'with_front' => true, ),
+    "show_admin_column" => false,
+    "show_in_rest" => true,
+    "rest_base" => "country",
+    "rest_controller_class" => "WP_REST_Terms_Controller",
+    "show_in_quick_edit" => false,
+    );
+register_taxonomy( "country", array( "sosa_dog" ), $args );
+
+/**
+ * Taxonomy: Size.
+ */
+
+$labels = array(
+    "name" => __( "Size", "sosa" ),
+    "singular_name" => __( "Size", "sosa" ),
+);
+
+$args = array(
+    "label" => __( "Size", "sosa" ),
+    "labels" => $labels,
+    "public" => true,
+    "publicly_queryable" => true,
+    "hierarchical" => true,
+    "show_ui" => true,
+    "show_in_menu" => true,
+    "show_in_nav_menus" => true,
+    "query_var" => true,
+    "rewrite" => array( 'slug' => 'size', 'with_front' => true, ),
+    "show_admin_column" => false,
+    "show_in_rest" => true,
+    "rest_base" => "size",
+    "rest_controller_class" => "WP_REST_Terms_Controller",
+    "show_in_quick_edit" => false,
+    );
+register_taxonomy( "size", array( "sosa_dog" ), $args );
 }
 add_action( 'init', 'cptui_register_my_taxes' );
